1d974f1cd9e6cd818edd8d9161f2195cfa1484dc
Fellipe Cocco <156376968+Fellipe-1Cocco2@users.noreply.github.com>
2025-09-12 18:22:06 -0300

    projeto atualizado
    projeto atualizado

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,7 @@
-const User = require('../models/User');
+const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcrypt');
+
+const prisma = new PrismaClient();
 
 // Controlador de usuários
 const UserController = {
@@ -8,15 +11,23 @@ const UserController = {
             // Extrai os dados do corpo da requisição
             const { name, email, password } = req.body;
 
-            // Cria uma nova instância do modelo User
-            const newUser = new User({ name, email, password });
+            // Criptografa a senha antes de salvar
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
 
-            // Salva o novo usuário no banco de dados
-            await newUser.save();
+            // Salva o novo usuário no banco de dados usando o Prisma
+            const newUser = await prisma.user.create({
+                data: {
+                    name,
+                    email,
+                    password: hashedPassword,
+                },
+            });
 
             // Retorna uma resposta de sucesso
-            res.status(201).json({ message: 'Usuário cadastrado com sucesso.' });
+            res.status(201).json({ message: 'Usuário cadastrado com sucesso.', user: newUser });
         } catch (error) {
+            console.error('Erro ao cadastrar usuário:', error);
             // Trata erros e retorna uma resposta de erro
             res.status(500).json({ error: 'Erro ao cadastrar usuário.' });
         }
--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const cors = require('cors');
-const connectDB = require('./config/db');
-const userRoutes = require('./routes/userRoutes');
+const path = require('path');
+const { PrismaClient } = require('@prisma/client');
+require('dotenv').config();
 
 const app = express();
+const prisma = new PrismaClient();
 
-// Conectar ao banco de dados
-connectDB();
+// Importe as novas rotas
+const userRoutes = require('./routes/userRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+const gameRoutes = require('./routes/gameRoutes');
+const pageRoutes = require('./routesPages/pagesRoutes');
+const e = require('express');
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', pageRoutes);
+
 // Rotas
 app.use('/api/users', userRoutes);
+app.use('/api/admin', adminRoutes);
+app.use('/api/game', gameRoutes);
 
 // Middleware de tratamento de erros
 app.use((err, req, res, next) => {
